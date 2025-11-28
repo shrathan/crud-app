6d8a6f899115d6134293389668fd1279731e9318
shrathan <shrathankumar15@gmail.com>
2025-11-28 14:15:44 +0530

    backend issue

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   url: process.env.MONGO_URI || `mongodb://root:password@mongo:27017/crud-db?authSource=admin`
 };
+console.log("Connecting to MongoDB at:", module.exports.url);
 
